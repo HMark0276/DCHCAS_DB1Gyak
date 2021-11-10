202d74867993b2aeba73a5e0fe3bd0371dea8681
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-11-10 10:37:15 +0100

    Update dchcas_7.6.sql

--- DCHCAS_1027/dchcas_7.6.sql
+++ DCHCAS_1027/dchcas_7.6.sql
@@ -9,6 +9,6 @@ ear INT CHECK (EAR > 0),
 kategoria CHAR(20)
 ); 
 
-Insert into termekek values(1, 'citrom', 15, 'C'); /*rekord felvitele */
-Delete from termekek where Ear<X; /* Az X-nél olcsóbb termékek törlése*/
-Update termekek set ear= ear*0.9 where kategoria=Y; /*az Y kategóriájú termékek árának csökkentése 10%-al */
+Insert into termekek values(1, 'citrom', 15, 'C');
+Delete from termekek where Ear<X;
+Update termekek set ear= ear*0.9 where kategoria=Y;
