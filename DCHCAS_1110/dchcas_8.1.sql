57649080df03dcda73382164d77047ccdd3c959d
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-11-10 13:24:58 +0100

    Update dchcas_8.1.sql

--- DCHCAS_1110/dchcas_8.1.sql
+++ DCHCAS_1110/dchcas_8.1.sql
@@ -35,5 +35,5 @@ insert into auto values('KAP-290','BMW 316','Fekete',6,2350,102);
 insert into auto values('GDF-525','Renault Twingo','Fekete',16,280,NULL);
 insert into auto values('HUB-936','Suzuki Swift','Fekete',16,500,NULL);
 
-SELECT auto.rendszam tulajdonos.nev;
+SELECT rendszam FROM auto AND nev FROM tulajdonos;
 
