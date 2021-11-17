47e5648f48e8bfda44c697e605dfa928b6158f18
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-11-17 12:39:55 +0100

    Create dchcas_8.sql

--- DCHCAS_1109/dchcas_8.sql
+++ DCHCAS_1109/dchcas_8.sql
@@ -17,18 +17,3 @@ create table termek(
 	ear INT CHECK(ear >0),
     gyarto INT(3) REFERENCES gyarto(adoszam)
 );
-
-create table komponens(
-	termek INT(20) REFERENCES termek(tkod),
-    alkatresz INT(20) REFERENCES alkatresz(akod)	
-);
-
-create table alkatresz(
-	akod INT(30),
-    nev VARCHAR(50)
-);
-
-create table egysegek(
-	aru INT(30) REFERENCES termek(tkod),
-    db INT CHECK(db>0)
-);
