b17423f07dfdd994e4f8568757ed78e5b113596e
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-11-24 09:25:36 +0100

    Update beadando.sql

--- DBSemTaskDCHCAS/beadando.sql
+++ DBSemTaskDCHCAS/beadando.sql
@@ -1,92 +1,97 @@
-create database beadando;
+create database dchcas;
 
-use beadando;
+use dchcas;
 
 create table Megnevezes(
-	fajnev VARCHAR(30) PRIMARY KEY,
+	ID INT(1) NOT NULL PRIMARY KEY,
+    fajnev VARCHAR(30),
 	tudom_nev VARCHAR(30),
 	tudom_alcsalad VARCHAR(30),
 	alcsalad VARCHAR(30)
 );
 
 create table Terulet(
-	szarm_terulet VARCHAR(30),
+	T_ID INT(1) NOT NULL PRIMARY KEY,
+    szarm_terulet VARCHAR(30),
 	jelen_terulet VARCHAR(30),
 	ved_statusz BOOLEAN,
 	kihalas_ideje VARCHAR(10),
-	Foreign key(fajnev) references Megnevezes(fajnev)
+	Foreign key(ID) references Megnevezes(ID)
 );
 
 create table Megjelenes(
-	meret INT(3),
+	M_ID INT(1) NOT NULL PRIMARY KEY,
+    meret INT(3),
 	suly INT(3),
 	minta VARCHAR(30),
 	szin VARCHAR(20),
-	Foreign key(fajnev) references Megnevezes(fajnev)
+	Foreign key(ID) references Megnevezes(ID)
 );
 
 create table Ragadozo(
+	R_ID INT NOT NULL PRIMARY KEY,
 	vadasz_idoszak VARCHAR(20),
 	territorium VARCHAR(20),
 	koz_statusz VARCHAR(20),
 	preda VARCHAR(20),
-	Foreign key(fajnev) references Megnevezes(fajnev)
+	Foreign key(ID) references Megnevezes(ID)
 );
 
 create table Utodjai(
+	U_ID INT(1) NOT NULL PRIMARY KEY,
 	himek_versengese VARCHAR(20),
 	vemh_ideje INT CHECK(vemh_ideje > 0),
 	kolykok_szama INT CHECK(kolykok_szama > 0),
 	tulelo_utodok_szama INT CHECK(tulelo_utodok_szama > 0),
-	Foreign key(fajnev) references Megnevezes(fajnev)
+	Foreign key(ID) references Megnevezes(ID)
 );
 
-insert into Megnevezes values('Sziberiai tigris','Panthera tigris altaica','Parducformak','Pantherinae');
-insert into Megnevezes values('Del-kinai tigris','Panthera tigris amoyensis','Parducformak','Pantherinae');
-insert into Megnevezes values('Bali tigris','Panthera tigris balica','Parducformak','Pantherinae');
-insert into Megnevezes values('Indokinai tigris','Panthera tigris corbetti','Parducformak','Pantherinae');
-insert into Megnevezes values('Malaj tigris','Panthera tigris jacksoni','Parducformak','Pantherinae');
-insert into Megnevezes values('Javai tigris','Panthera tigris sondaica','Parducformak','Pantherinae');
-insert into Megnevezes values('Szumatrai tigris','Panthera tigris sumatrae','Parducformak','Pantherinae');
-insert into Megnevezes values('Bengali tigris','Panthera tigris tigris','Parducformak','Pantherinae');
-insert into Megnevezes values('Kaszpi tigris','Panthera tigris virgata','Parducformak','Pantherinae');
+insert into Megnevezes values(1,'Sziberiai tigris','Panthera tigris altaica','Parducformak','Pantherinae');
+insert into Megnevezes values(2,'Del-kinai tigris','Panthera tigris amoyensis','Parducformak','Pantherinae');
+insert into Megnevezes values(3,'Bali tigris','Panthera tigris balica','Parducformak','Pantherinae');
+insert into Megnevezes values(4,'Indokinai tigris','Panthera tigris corbetti','Parducformak','Pantherinae');
+insert into Megnevezes values(5,'Malaj tigris','Panthera tigris jacksoni','Parducformak','Pantherinae');
+insert into Megnevezes values(6,'Javai tigris','Panthera tigris sondaica','Parducformak','Pantherinae');
+insert into Megnevezes values(7,'Szumatrai tigris','Panthera tigris sumatrae','Parducformak','Pantherinae');
+insert into Megnevezes values(8,'Bengali tigris','Panthera tigris tigris','Parducformak','Pantherinae');
+insert into Megnevezes values(9,'Kaszpi tigris','Panthera tigris virgata','Parducformak','Pantherinae');
 
-insert into Terulet values('Kina','Sziberia',1,'nincs','Sziberiai tigris');
-insert into Terulet values('Kina','Kina',1,'nincs','Del-kinai tigris');
-insert into Terulet values('Bali','nincs',0,'1937','Bali tigris');
-insert into Terulet values('Thaifold','Vietnam',1,'nincs','Indokinai tigris');
-insert into Terulet values('Malajzia','Malajzia',1,'nincs','Malaj tigris');
-insert into Terulet values('Java','nincs',0,'1980','Javai tigris');
-insert into Terulet values('Malajzia','Indonezia',1,'nincs','Szumatrai tigris');
-insert into Terulet values('India','Sundarbans',1,'nincs','Bengali tigris');
-insert into Terulet values('Kina','Sziberia',0,'1996','Kaszpi tigris');
+insert into Terulet values(1,'Kina','Sziberia',1,'nincs');
+insert into Terulet values(2,'Kina','Kina',1,'nincs');
+insert into Terulet values(3,'Bali','nincs',0,'1937');
+insert into Terulet values(4,'Thaifold','Vietnam',1,'nincs');
+insert into Terulet values(5,'Malajzia','Malajzia',1,'nincs');
+insert into Terulet values(6,'Java','nincs',0,'1980');
+insert into Terulet values(7,'Malajzia','Indonezia',1,'nincs');
+insert into Terulet values(8,'India','Sundarbans',1,'nincs');
+insert into Terulet values(9,'Kina','Sziberia',0,'1996');
 
-insert into Megjelenes values(310,220,'vastag csikok','sotet sarga','Sziberiai tigris');
-insert into Megjelenes values(250,150,'keskeny csikok','sarga','Del-kinai tigris');
-insert into Megjelenes values(170,80,'keskeny csikok','narancs','Bali tigris');
-insert into Megjelenes values(250,200,'keskeny csikok','sotet sarga','Indokinai tigris');
-insert into Megjelenes values(200,100,'fekete-feher csikok','sarga','Malaj tigris');
-insert into Megjelenes values(250,120,'keskeny csikok','narancs','Javai tigris');
-insert into Megjelenes values(220,110,'keskeny csikok','sotet sarga','Szumatrai tigris');
-insert into Megjelenes values(330,260,'vastag csikok',' vilagos sarga','Bengali tigris');
-insert into Megjelenes values(290,240,'keskeny csikok','voros','Kaszpi tigris');
+insert into Megjelenes values(1,310,220,'vastag csikok','sotet sarga');
+insert into Megjelenes values(2,250,150,'keskeny csikok','sarga');
+insert into Megjelenes values(3,170,80,'keskeny csikok','narancs');
+insert into Megjelenes values(4,250,200,'keskeny csikok','sotet sarga');
+insert into Megjelenes values(5,200,100,'fekete-feher csikok','sarga');
+insert into Megjelenes values(6,250,120,'keskeny csikok','narancs');
+insert into Megjelenes values(7,220,110,'keskeny csikok','sotet sarga');
+insert into Megjelenes values(8,330,260,'vastag csikok',' vilagos sarga');
+insert into Megjelenes values(9,290,240,'keskeny csikok','voros');
 
-insert into Ragadozo values('ejszaka','fuves terulet','maganyos','nagytestu novenyevok','Sziberiai tigris');
-insert into Ragadozo values('barmikor','sziklas terulet','maganyos','patasok','Del-kinai tigris');
-insert into Ragadozo values('ejszaka','erdos resz','maganyos','kozepes emlosok','Bali tigris');
-insert into Ragadozo values('barmikor','magas fuves terulet','maganyos','kozepes emlosok','Indokinai tigris');
-insert into Ragadozo values('ejszaka','magas fuves terulet','maganyos','kozepes emlosok','Malaj tigris');
-insert into Ragadozo values('barmikor','erdos resz','maganyos','patasok','Javai tigris');
-insert into Ragadozo values('ejszaka','sziklas terulet','maganyos','kozepes emlosok','Szumatrai tigris');
-insert into Ragadozo values('ejszaka','erdos resz','maganyos','nagyobb emlosok','Bengali tigris');
-insert into Ragadozo values('nappal','magas fuves terulet','maganyos','nagytestu novenyevok','Kaszpi tigris');
+insert into Ragadozo values(1,'ejszaka','fuves terulet','maganyos','nagytestu novenyevok');
+insert into Ragadozo values(2,'barmikor','sziklas terulet','maganyos','patasok');
+insert into Ragadozo values(3,'ejszaka','erdos resz','maganyos','kozepes emlosok');
+insert into Ragadozo values(4,'barmikor','magas fuves terulet','maganyos','kozepes emlosok');
+insert into Ragadozo values(5,'ejszaka','magas fuves terulet','maganyos','kozepes emlosok');
+insert into Ragadozo values(6,'barmikor','erdos resz','maganyos','patasok');
+insert into Ragadozo values(7,'ejszaka','sziklas terulet','maganyos','kozepes emlosok');
+insert into Ragadozo values(8,'ejszaka','erdos resz','maganyos','nagyobb emlosok');
+insert into Ragadozo values(9,'nappal','magas fuves terulet','maganyos','nagytestu novenyevok');
 
-insert into Utodjai values('agressziv',3,6,2,'Sziberiai tigris');
-insert into Utodjai values('agressziv',3,6,3,'Del-kinai tigris');
-insert into Utodjai values('nem tudni',4,3,1,'Bali tigris');
-insert into Utodjai values('passziv',4,5,2,'Indokinai tigris');
-insert into Utodjai values('agressziv',3,6,3,'Malaj tigris');
-insert into Utodjai values('nem tudni',3,3,1,'Javai tigris');
-insert into Utodjai values('passziv',3,7,3,'Szumatrai tigris');
-insert into Utodjai values('agressziv',3,4,3,'Bengali tigris');
-insert into Utodjai values('nem tudni',4,3,1,'Kaszpi tigris');
+insert into Utodjai values(1,'agressziv',3,6,2);
+insert into Utodjai values(2,'agressziv',3,6,3);
+insert into Utodjai values(3,'nem tudni',4,3,1);
+insert into Utodjai values(4,'passziv',4,5,2);
+insert into Utodjai values(5,'agressziv',3,6,3);
+insert into Utodjai values(6,'nem tudni',3,3,1);
+insert into Utodjai values(7,'passziv',3,7,3);
+insert into Utodjai values(8,'agressziv',3,4,3);
+insert into Utodjai values(9,'nem tudni',4,3,1);
