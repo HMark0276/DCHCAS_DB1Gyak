2feb692599f6d77382f58368dcc6f066dfbc0d00
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-11-24 13:41:23 +0100

    Update dchcas_8.sql

--- DCHCAS_1109/dchcas_8.sql
+++ DCHCAS_1109/dchcas_8.sql
@@ -5,17 +5,17 @@ use dchcas_8;
 create table gyarto(
 	adoszam INT PRIMARY KEY,
 	nev VARCHAR(30),
-    telephely VARCHAR(30),
-    irsz VARCHAR(4),
+	telephely VARCHAR(30),
+	irsz VARCHAR(4),
 	varos VARCHAR(40),
-    utca VARCHAR(100)
+	utca VARCHAR(100)
 );
 
 create table termek(
 	tkod INT(20) PRIMARY KEY,
 	nev VARCHAR(50),
 	ear INT CHECK(ear >0),
-    gyarto INT(3) REFERENCES gyarto(adoszam)
+	gyarto INT(3) REFERENCES gyarto(adoszam)
 );
 2.RESZ
 create table egysegek(
