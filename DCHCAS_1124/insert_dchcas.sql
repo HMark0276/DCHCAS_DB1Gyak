f75d9f4d7b39b8627b03523f8c4cf47f31828461
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-12-01 12:32:26 +0100

    Create insert_dchcas.sql

---
+++ DCHCAS_1124/insert_dchcas.sql
@@ -0,0 +1,18 @@
+
+insert into oktato values('FVH884', 'Istvan', 'Matematika');
+insert into oktato values('H4GMNV', 'Gabor', 'Matematika');
+insert into oktato values('3CF4TJ', 'Janos', 'Informatika');
+insert into oktato values('2GGV45', 'Eva', 'Gazdasag');
+insert into oktato values('34CV5V', 'Anna', 'Matematika');
+
+insert into targy values(123,4,'Valszam');
+insert into targy values(111,3,'Gazdjog');
+insert into targy values(132,2,'Progelm');
+insert into targy values(544,6,'db1');
+insert into targy values(765,5,'Oprendszerek');
+
+insert into hallgato values('34CAA3','Adam','GEIK','1999',22,3213,'Miskolc','Petofi',23);
+insert into hallgato values('V45GG2','Noemi','GTK','2001',20,3422,'Miskolc','Hajdu',3);
+insert into hallgato values('V2SD55','Reka','AJK','2000',21,3211,'Miskolc','Erzsebet',7);
+insert into hallgato values('VTQTDF','Vencel','AJK','2000',21,3433,'Miskolc','Ady',4);
+insert into hallgato values('2C3545','Daniel','GEIK','1999',22,3521,'Miskolc','Arviz',11);
