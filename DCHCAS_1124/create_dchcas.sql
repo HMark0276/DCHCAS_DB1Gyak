de0d50ef20a4f3abe4f85d7b00392a13861687ba
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-12-01 12:32:08 +0100

    Update create_dchcas.sql

--- DCHCAS_1124/create_dchcas.sql
+++ DCHCAS_1124/create_dchcas.sql
@@ -18,7 +18,7 @@ create table hallgato(
 	neptunkod VARCHAR(6) PRIMARY KEY,
 	nev VARCHAR(20),
 	tankor VARCHAR(20),
-	szul_d DATE,
+	szul_d VARCHAR(20),
 	eletkor INT(2),
 	irszam INT(4),
 	varos VARCHAR(20),
@@ -35,17 +35,3 @@ create table hallgat(
 	FOREIGN KEY(kod) references targy(kod),
 	FOREIGN KEY(neptunkod) references hallgato(kod);
 );
-
-insert into oktato values('FVH884', 'Istvan', 'Matematika');
-insert into oktato values('H4GMNV', 'Gabor', 'Matematika');
-insert into oktato values('3CF4TJ', 'Janos', 'Informatika');
-insert into oktato values('2GGV45', 'Eva', 'Gazdasag');
-insert into oktato values('34CV5V', 'Anna', 'Matematika');
-
-insert into targy values('123','4','Valszam');
-insert into targy values('111','3','Gazdjog');
-insert into targy values('132','2','Progelm');
-insert into targy values('544','6','db1');
-insert into targy values('765','5','Oprendszerek');
-
-insert into hallgato values('
