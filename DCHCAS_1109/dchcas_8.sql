9dad950a03b86402b63f7d90f66ce3b6d0ccf3ff
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-12-03 11:58:11 +0100

    Update dchcas_8.sql

--- DCHCAS_1109/dchcas_8.sql
+++ DCHCAS_1109/dchcas_8.sql
@@ -66,52 +66,74 @@ insert into alkatresz values(03,'kupak');
 insert into alkatresz values(03,'doboz');
 insert into alkatresz values(05,'uveg');
 
-select * from termek where ear>3000;
-select nev, adoszam from gyarto where telephely='Gyor';
-select count(tkod) from termek where ear>3000;
-select tkod from termek where nev='tej';
-select nev from gyarto where telephely=NULL;
+select * from termek where ear>1000;
+select gyarto.nev, gyarto.adoszam from gyarto where telephely like 'Gyor%';
+select count(termek.nev) from termek where ear>3000;
+select termek.tkod from termek where termek.nev like 'kenyer';
+select gyarto.nev from gyarto where telephely is NULL;
 2.RESZ
-select termek.nev,gyarto.nev from termek,gyarto;
-select termek.nev,gyarto.nev,ear from termek,gyarto, termek;
-select termek.nev,alkatresz.nev from termek,alkatresz;
-select akod from alkatresz where akod=NULL;
+select termek.nev, gyarto.nev from termek, gyarto;
+select termek.nev, termek.ear from termek where gyartok.nev = 'A';
+select alkatresz.nev from alkatresz where termek.nev = 'fa';
+select termek.nev from termek where alkatresz.akod and komponens.akod is null;
 3.RESZ
-create table tanfolyam(
-	tkod INT PRIMARY KEY,
-	ar INT CHECK(ar>0),
-	megnevezes VARCHAR(50),
-	tipus VARCHAR(1)
+create table resztvevo (
+tajszam varchar(12) primary key,
+nev varchar(50),
+lakcim varchar(100)
 );
 
-create table befizetes(
-	kurzus INT NOT NULL,
-	diak BOOLEAN,
-	befizetes INT,
-	FOREIGN KEY(kurzus) references tanfolyam(tkod),
-	FOREIGN KEY(diak) references resztvevo(tajszam)
+create table tanfolyam (
+tkod int primary key,
+ar int check(ar>0),
+megnevezes varchar(50),
+tipus varchar(1)
 );
 
-create table resztvevo(
-	tajszam VARCHAR(12) PRIMARY KEY,
-	nev VARCHAR(50),
-	lakcim VARCHAR(100)
+create table befizetes (
+kurzus int,
+diak boolean not null default 0,
+befizetes int,
+foreign key (kurzus) references tanfolyam(tkod),
+foreign key (diak) references resztvevo(tajszam)
 );
 
-insert into tanfolyam(10,60000,'informatika',1);
-insert into tanfolyam(20,40000,'matematika',1);
-insert into tanfolyam(30,25000,'geodezia',2);
-insert into tanfolyam(40,15000,'gazdasagtan',3);
-insert into tanfolyam(50,40000,'logisztika',4);
+insert into resztvevo values('1','Jozsef',87262344,'Arviz utca 1');
+insert into resztvevo values('2','Agnes',7233363,'Negyesi utca 52');
+insert into resztvevo values('3','Istvan',78233663,'Virag utca 111');
+insert into resztvevo values('4','Benedek',79122121,'Oz utca 2');
+insert into resztvevo values('5','Akos',62244121,'Ady utca 54');
 
-insert into befizetes(10,1,30000);
-insert into befizetes(20,1,15000);
-insert into befizetes(30,1,20000);
-insert into befizetes(30,1,10000);
-insert into befizetes(40,1,10000);
+insert into tanfolyam values(1,'5000','A','E2');
+insert into tanfolyam values(2,'5000','A','E1');
+insert into tanfolyam values(3,'2000','B','K2');
+insert into tanfolyam values(4,'2000','B','K1');
 
-insert into resztvevo('abc1','Istvan','Miskolc, Ady utca');
-insert into resztvevo('abc2','Balazs','Budapest, Ar utca');
-insert into resztvevo('abc3','Laura','Miskolc, Petofi utca');
-insert into resztvevo('abc4','Dezso','Debrecen, Kossuth utca');
-insert into resztvevo('abc5','Agnes','Miskolc, Erenyo utca');
+
+insert into befizetes values(1, 87262344, 3000);
+insert into befizetes values(1, 7233363, 1000);
+insert into befizetes values(2, 78233663, 7000);
+insert into befizetes values(3, 79122121, 2500);
+insert into befizetes values(4, 78233663, 1000);
+insert into befizetes values(3, 62244121, 5500);
+insert into befizetes values(4, 7233363, 500);
+
+select tipus, count(*) db from tanfolyam group by tipus;
+select kurzus, count(*) db from befizetes group by kurzus;
+select t.nev, count(*) db from befizetes b INNER join tanfolyam t on b.kurzus = t.tkod;
+select t.nev, count(*) db from befizetes b INNER join tanfolyam t on ;b.kurzus = t.tkod where b.befizetes = t.ar;
+select r.lakcim, count(*) db from befizetes b INNER join resztvevo r on b.diak = r.tajszam where b.kurzus IN (select tkod from tanfolyam where megnevezes = ‘A’);
+
+4.RESZ
+select * from termek where ear>1000;
+select gyarto.nev, gyarto.adoszam from gyarto where telephely like 'Gyor%';
+select count(termek.nev) from termek where ear>3000;
+select termek.tkod from termek where termek.nev like 'kenyer';
+select gyarto.nev from gyarto where telephely is NULL;
+
+select count(*) from termek where ear > (select avg(ear) from termek);
+select nev from termek where ear = (select max(ear) from termek);
+select nev from gyarto where adoszam in (select gyarto from termek where ear = (select max(ear) from termek));
+create view gyatlag as select gy.nev, AVG(gy.ear) mear from gyarto t inner join termek t on t.gyarto = gy.adoszam group by gy.nev;
+select nev from gyatlag where mear = (select max(mear) from gyatlag);
+select gy.nev, avg (t.ear) aear from gyarto gy left outer join termek t ON gy.adoszam = t.gyarto group by gy,nev;
